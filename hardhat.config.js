bfcf2b33042470bb7668b953ed3fb0e69d7a6437
santeangelo46-hash <santeangelo46@gmail.com>
2025-08-26 05:28:55 +0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,3 +11,34 @@ module.exports = {
     },
   },
 };
+
+name: Deploy to SKALE Europa Hub
+
+on:
+  push:
+    branches: [ "main" ]
+
+jobs:
+  deploy:
+    runs-on: ubuntu-latest
+
+    steps:
+      - name: Checkout repo
+        uses: actions/checkout@v3
+
+      - name: Setup Node.js
+        uses: actions/setup-node@v3
+        with:
+          node-version: 18
+
+      - name: Install dependencies
+        run: |
+          npm install
+
+      - name: Deploy contracts to SKALE
+        env:
+          SKALE_RPC_URL: ${{ secrets.SKALE_RPC_URL }}
+          SKALE_CHAIN_ID: ${{ secrets.SKALE_CHAIN_ID }}
+          DEPLOYER_PRIVATE_KEY: ${{ secrets.DEPLOYER_PRIVATE_KEY }}
+        run: |
+          npx hardhat run scripts/deploy.js --network skale
